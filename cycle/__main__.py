7a91778817d5e6d7982b63e5032bccb57ca7a4a4
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-06-23 03:11:46 +0530

    Commit game over and main for player scoring

--- cycle/__main__.py
+++ cycle/__main__.py
@@ -18,6 +18,35 @@ from game.shared.point import Point
 
 def main():
     
+    # Creates two cycles, gets their position and color
+    cycle_one = Cycle(
+        Point(int(constants.MAX_X - 600), int(constants.MAX_Y / 2)))
+    cycle_two = Cycle(
+        Point(int(constants.MAX_X - 300), int(constants.MAX_Y / 2)))
+    cycle_one.set_cycle_color(constants.GREEN)
+    cycle_two.set_cycle_color(constants.RED)
+   #  cycle_one_name = input("\n\nPlease enter player 1 name: " + "\n\n")
+   # cycle_two_name = input("\n\nPlease enter player 2 name: " + "\n\n")
+    cycle_one_name = "AA"
+    cycle_two_name = "BB"
+    cycle_one.set_name(cycle_one_name)
+    cycle_two.set_name(cycle_two_name)
+
+    # Create the cast
+    cast = Cast()
+    score1 = Score()
+    score2 = Score()
+    score1.set_position(Point(constants.MAX_X - 850, 0))
+    score2.set_position(Point(constants.MAX_X - 200, 0))
+    score1.add_points(3)
+    score2.add_points(3)
+    score1.set_player_name(cycle_one_name)
+    score2.set_player_name(cycle_two_name)
+    cast.add_actor("cycle_one", cycle_one)
+    cast.add_actor("cycle_two", cycle_two)
+    cast.add_actor("score1", score1)
+    cast.add_actor("score2", score2)
+    
     # create the cast
     cast = Cast()
     cast.add_actor("foods", Food())
