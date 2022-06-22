aba74b0140cde4f67adbd73e1b5406c548b079de
karrassphiri <97294565+karrassphiri@users.noreply.github.com>
2022-06-23 00:29:12 +0200

    fix loading 2

--- cycle/game/scripting/draw_actors_action.py
+++ cycle/game/scripting/draw_actors_action.py
@@ -26,18 +26,18 @@ class DrawActorsAction(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        score1 = cast.get_first_actor("score1")  # P1
-        score2 = cast.get_first_actor("score2")  # P2
-        cycle_one = cast.get_first_actor("cycle_one")  # P1
-        cycle_two = cast.get_first_actor("cycle_two")  # P2
+        score1 = cast.get_first_actor("score1") #P1
+        score2 = cast.get_first_actor("score2") #P2
+        cycle_one = cast.get_first_actor("cycle_one") #P1
+        cycle_two = cast.get_first_actor("cycle_two") #P2
         segments_one = cycle_one.get_segments()  # P1
         segments_two = cycle_two.get_segments()  # P2
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
-        self._video_service.draw_actors(segments_one)  # P1
-        self._video_service.draw_actors(segments_two)  # P2
-        self._video_service.draw_actor(score1)  # P1
-        self._video_service.draw_actor(score2)  # P2
+        self._video_service.draw_actors(segments_one) #P1
+        self._video_service.draw_actors(segments_two) #P2
+        self._video_service.draw_actor(score1) #P1
+        self._video_service.draw_actor(score2) #P2
         self._video_service.draw_actors(messages, True)
         self._video_service.flush_buffer()
